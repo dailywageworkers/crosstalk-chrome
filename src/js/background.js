eb4813f3978cd9a9026b3dfcaaa0409c2b156850
Aravind Balla <bsaaravind@gmail.com>
2017-10-29 22:55:48 +0530

    Add Firebase start

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,11 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+const config = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
